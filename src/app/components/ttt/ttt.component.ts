4b92ccf5e97deb52fe60e7fe20a656d1f04f4a0f
Lukáš Vrba <lukasvrbaa@seznam.cz>
2021-01-20 17:24:35 +0100

    game prototype

--- src/app/components/ttt/ttt.component.ts
+++ src/app/components/ttt/ttt.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {LoginService} from "../../services/login.service";
 import {webSocket} from "rxjs/webSocket";
 import {Game} from "../../models/game";
+import {Gameplay} from "../../models/gameplay";
 
 @Component({
   selector: 'app-ttt',
@@ -12,34 +13,39 @@ import {Game} from "../../models/game";
 export class TttComponent implements OnInit {
   numbers: any;
   ws = new WebSocket('ws://localhost:8081/api/notify')
-  //ws = webSocket('ws://localhost:8081/api/gaming')
+  gameWs = new WebSocket('ws://localhost:8081/api/gaming')
   gameId: number = 0;
   waiting = false;
   isGameOn = false;
+  currentGamePlay: Gameplay = {}
   public currentUser = this.loginService.currentUserValue
+  notCurrentlyPlaying = true;
 
   constructor(private loginService: LoginService) {
-    this.numbers = Array.from(Array(20)).map((x, i) => i );
+    this.numbers = Array.from(Array(20)).map((x, i) => i);
     //window.onunload = function () {
     //  return loginService.silentLogout();
     //};
   }
 
   ngOnInit(): void {
+    // @ts-ignore
+
+
     this.ws.onmessage = (message) => {
-      let game:Game = JSON.parse(message.data);
-      if (game.isGameOn){
+      let game: Game = JSON.parse(message.data);
+      if (game.isGameOn) {
         this.isGameOn = true;
         this.waiting = false;
         return
       }
-      if (game.user1 === this.currentUser.username){
-        this.join(game.id,game.user1)
+      if (game.user1 === this.currentUser.username) {
+        this.join(game.id, game.user1, game.user2)
         this.waiting = true;
       }
-      if (game.user2 === this.currentUser.username){
+      if (game.user2 === this.currentUser.username) {
         if (confirm(game.user1 + " asked you to play, Do you want to join?")) {
-          this.join(game.id,game.user2)
+          this.join(game.id, game.user1, game.user2)
           this.waiting = false;
           this.isGameOn = true;
           game.isGameOn = true;
@@ -50,18 +56,59 @@ export class TttComponent implements OnInit {
       }
     }
   }
-  join(id: number, username: string): void {
+
+  join(id: number, user1: string, user2: string): void {
     this.gameId = id;
-    // @ts-ignore
-    document.getElementById('myTable').addEventListener('click', function(){
-    // @ts-ignore
-    let col = window.event.target.cellIndex;
-    // @ts-ignore
-    let row = window.event.target.parentNode.rowIndex;
-    // @ts-ignore
-      let x=document.getElementById('myTable').rows[row].cells;
-      x[col].innerHTML="<span style='disabled'>X</span>";
-    alert('Col index is: ' + col + '\nRow index is: ' + row);
-    }, false);
+    this.currentGamePlay.id = id
+    this.currentGamePlay.user = user1
+    this.currentGamePlay.isWinner = false
+    this.currentGamePlay.y = -1;
+    this.currentGamePlay.x = -1;
+    this.currentGamePlay.shape = "X"
+    this.play(this.currentGamePlay)
+    this.gameWs.onmessage = (message) => {
+      let move: Gameplay = JSON.parse(message.data);
+      if (move.id === this.currentGamePlay.id) {
+        // @ts-ignore
+        let x = document.getElementById('myTable').rows[move.x].cells;
+        x[move.y].innerHTML = "<span style='disabled'>" + move.shape + "</span>";
+        if (move.user === user1) {
+          this.currentGamePlay.user = user2;
+          this.currentGamePlay.shape = "O";
+        } else {
+          this.currentGamePlay.user = user1;
+          this.currentGamePlay.shape = "X"
+        }
+        if (!this.currentGamePlay.isWinner){
+          if (move.user === this.currentUser.username){
+            document.addEventListener("click",this.handler,true);
+          }else {
+            console.log(this.currentGamePlay)
+            console.log(this.currentUser.username)
+            this.play(this.currentGamePlay)
+          }
+        }
+        }
+
+      }
+
+    }
+  play (currentGamePlay: Gameplay): void {
+      document.removeEventListener('click', this.handler)
+      // @ts-ignore
+      document.getElementById('myTable').addEventListener('click', () => {
+        // @ts-ignore
+        currentGamePlay.y = window.event.target.cellIndex;
+        // @ts-ignore
+        currentGamePlay.x = window.event.target.parentNode.rowIndex;
+        if (currentGamePlay.y !== -1 || currentGamePlay.x !== -1) {
+          this.gameWs.send(JSON.stringify(currentGamePlay))
+        }
+      }, false);
+
+  }
+  handler(e: { stopPropagation: () => void; preventDefault: () => void; }){
+    e.stopPropagation();
+    e.preventDefault();
   }
 }
